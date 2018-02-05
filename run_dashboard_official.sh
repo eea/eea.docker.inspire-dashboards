0edfaf7ad2c23b724de01ea201595993e2c0902e
doublebyte1 <doublebyte@gmail.com>
2018-02-05 10:25:51 +0100

    - set explicit name to sysctl container

--- run_dashboard_official.sh
+++ run_dashboard_official.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo sysctl -w vm.max_map_count=262144
 docker-compose -p dashboard-official -f docker-compose-canonical.yml -f docker-compose-eea-dashboard-official.yml up -d
--- run_dashboard_sandbox.sh
+++ run_dashboard_sandbox.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo sysctl -w vm.max_map_count=262144
 docker-compose -p dashboard-sandbox -f docker-compose-canonical.yml -f docker-compose-eea-dashboard-sandbox.yml up -d
